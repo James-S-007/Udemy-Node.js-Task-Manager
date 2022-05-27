bd598813531d491e48c2082e299b095729d55d7f
James-S-007 <jspringer8@gatech.edu>
2022-05-27 13:18:22 -0400

    Fixtures

---
+++ tests/fixtures/db.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Mike',
+    email: 'mike@exaple.com',
+    password: 'teenagemutantninjaturtles',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'Jerry',
+    email: 'jerry@exaple.com',
+    password: 'jerryberry',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First task',
+    completed: false,
+    owner: userOne._id
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second task',
+    completed: true,
+    owner: userOne._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third task',
+    completed: true,
+    owner: userTwo._id
+}
+
+module.exports = {
+    userOneId,
+    userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree
+}
